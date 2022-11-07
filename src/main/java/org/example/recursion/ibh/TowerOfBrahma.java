bc26ac4c157553c5e81ceb8abc4423e1ca7432ae
junejaankit1995 <junejaankit1995@gmail.com>
2022-11-07 12:07:58 +0530

    Tower of Brahman variation

--- src/main/java/org/example/recursion/ibh/TowerOfBrahma.java
+++ src/main/java/org/example/recursion/ibh/TowerOfBrahma.java
@@ -15,9 +15,27 @@ public class TowerOfBrahma {
         printSteps(help, dest, sourc, n - 1);
     }
 
+
+    public long printAndCountSteps(int N, int from, int to, int aux) {
+        //Base Condition
+        long steps = 0;
+        if(N == 1) {
+            System.out.println("move disk " +N+ " from rod " +from+ " to rod " +to);
+            return 1;
+        }
+        //Hypothesis
+        steps += printAndCountSteps(N-1, from, aux, to);
+        //Induction
+        System.out.println("move disk " +N+ " from rod " +from+ " to rod " +to);
+        steps += 1;
+        return steps += printAndCountSteps(N-1, aux, to, from);
+
+    }
+
     public static void main(String[] args) {
         TowerOfBrahma obj = new TowerOfBrahma();
-        obj.printSteps("source", "destination", "helper", 3);
+        //obj.printSteps("source", "destination", "helper", 3);
+        obj.printAndCountSteps(3, 1, 2, 3);
     }
 
 
