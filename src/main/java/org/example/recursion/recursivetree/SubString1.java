14caa87d17a7511ec3a3ca6224553ec17948035b
junejaankit1995 <junejaankit1995@gmail.com>
2022-11-08 11:57:36 +0530

    SubString new Variation

--- src/main/java/org/example/recursion/recursivetree/SubString1.java
+++ src/main/java/org/example/recursion/recursivetree/SubString1.java
@@ -1,25 +1,10 @@
 package org.example.recursion.recursivetree;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubString1 {
-
-    void basicPrintSubSet(StringBuilder output, StringBuilder input) {
-        //Base Condition
-        if (input.length() == 0) {
-            System.out.println(output);
-            return;
-        }
-
-        StringBuilder output1 = new StringBuilder(output);
-        StringBuilder output2 = new StringBuilder(output);
-
-        output2.append(input.charAt(0));
-
-        StringBuilder input1 = new StringBuilder(input.substring(1));
-
-        printSubSet(output1, input1);
-        printSubSet(output2, input1);
-    }
 
     void printSubSet(StringBuilder output, StringBuilder input) {
         /*
@@ -51,12 +36,6 @@ public class SubString1 {
         printSubSet(new StringBuilder(output.append(input.charAt(0))), newInput);
     }
 
-    public static void main(String[] args) {
-        SubString1 obj = new SubString1();
-        //obj.printSubSet(new StringBuilder(""), new StringBuilder("ab"));
-        obj.printSubSet("", "abc");
-    }
-
     //Recursive Tree
     void printSubSet(String output, String input) {
         //Base Condition
@@ -67,6 +46,52 @@ public class SubString1 {
         printSubSet(output, input.substring(1));
         printSubSet(output + input.charAt(0), input.substring(1));
 
+    }
+
+    //Variation 2
+    public long printAndCount(String input, String output) {
+        long count = 0;
+        if(input.isEmpty()) {
+            System.out.println(output);
+            return 1;
+        }
+        count += printAndCount(input.substring(1), output);
+        return count += printAndCount(input.substring(1), output.concat(input.substring(0, 1)));
+    }
+
+    //Variation 3 URL : https://leetcode.com/problems/subsets/
+    public List<List<Integer>> subsets(int[] intArray) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> input = new ArrayList<>();
+        for(int val : intArray)
+            input.add(val);
+        getSubSet(input,  new ArrayList<>(), result);
+        return result;
+    }
+
+    public void getSubSet(List<Integer> input, List<Integer> output, List<List<Integer>> cont) {
+
+        //Base Condition
+        if(input.isEmpty()) {
+            cont.add(output);
+            return;
+        }
+        List<Integer> newInput = new ArrayList<>(input);
+        Integer temp = newInput.remove(0);
+        getSubSet(newInput,output, cont);
+        List<Integer> newOutput = new ArrayList<>(output);
+        newOutput.add(temp);
+        getSubSet(newInput, newOutput, cont);
+    }
+
+
+
+    public static void main(String[] args) {
+        SubString1 obj = new SubString1();
+        obj.printSubSet(new StringBuilder(), new StringBuilder("ab"));
+        obj.printSubSet("", "abc");
+        System.out.println(obj.printAndCount("", ""));
+        System.out.println(obj.subsets(new int[]{1, 2, 3}));
     }
 
 }
